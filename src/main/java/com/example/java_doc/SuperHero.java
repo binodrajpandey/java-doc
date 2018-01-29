24bbc0f18592b52e2caf9408a2741f85e185f864
Binod Raj Pandey <binodrajpandey@ioe.edu.np>
2018-01-29 08:55:23 +0545

    java-doc

---
+++ src/main/java/com/example/java_doc/SuperHero.java
@@ -0,0 +1,62 @@
+package com.example.java_doc;
+/**
+ * Hero is the main entity we are using to.........
+ * @author user
+ *
+ */
+public class SuperHero {
+	/**
+     * The public name of a hero that is common knowledge
+     */
+    private String heroName;
+    private String uniquePower;
+    private int health;
+    private int defense;
+	
+    /**
+     * <p>This is a simple description of the method. . .
+     * <a href="http://www.supermanisthegreatest.com">Superman!</a>
+     * </p>
+     * @param incomingDamage the amount of incoming damage
+     * @return the amount of health hero has after attack
+     * @see <a href="http://www.link_to_jira/HERO-402">HERO-402</a>
+     * @since 1.0
+     */
+    public int successfullyAttacked(int incomingDamage, String damageType) {
+    	// do things
+        return 0;
+    }
+
+	public String getHeroName() {
+		return heroName;
+	}
+
+	public void setHeroName(String heroName) {
+		this.heroName = heroName;
+	}
+
+	public String getUniquePower() {
+		return uniquePower;
+	}
+
+	public void setUniquePower(String uniquePower) {
+		this.uniquePower = uniquePower;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getDefense() {
+		return defense;
+	}
+
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+	
+}
